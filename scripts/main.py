86c27339969941a7e53446915f37873db31d3075
Erick Cohen <erickcohen@Ericks-MacBook-Air.local>
2022-09-08 16:55:38 -0400

    Adds __name__ = __main__ for main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,20 +1,27 @@
 import os
-import numpy as np
 import docx
 
 PATH_DATA_RAW = "./data-raw/"
 PATH_DATA_OUTPUT = "./data-output/"
 
-files = os.listdir(PATH_DATA_RAW)
-output = docx.Document()
+def combine_word_doc(
+    data_raw_path = PATH_DATA_RAW, 
+    data_output_path = PATH_DATA_OUTPUT
+    ):
+    
+    files = os.listdir(data_raw_path)
+    output = docx.Document()
 
-for file in files:
-    with open(file) as f:
-        text = f.read()
-        output.add_paragraph(text)
-        output.add_paragraph(" ")
+    for file in files:
+        with open(file) as f:
+            text = f.read()
+            output.add_paragraph(text)
+            output.add_paragraph(" ")
 
 
-output.save(str(PATH_DATA_OUTPUT) + "combined_file.docx")
+    output.save(str(data_output_path) + "combined_file.docx")
 
-print("Files combined. Please find result in 'data-output' folder")
+    print("Files combined. Please find result in 'data-output' folder")
+
+if __name__ == "__main__":
+    combine_word_doc()
